496fe71b121a2e204b618cd17225d8a7690de4b7
loftkun <lofkun@gmail.com>
2019-08-05 22:28:59 +0900

    add

---
+++ test/launch.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# build & launch
+sudo docker-compose up --build
+
+# test
+#CONTAINER=simplemsaapps_app1_1
+
+#sudo docker exec -it ${CONTAINER} curl localhost:5000
+
+#sudo docker exec -it ${CONTAINER} curl localhost:5000?sleep=0
+
+#sudo docker exec -it ${CONTAINER} curl localhost:5000?sleep=2
+
+#sudo docker exec -it ${CONTAINER} curl localhost:5000?sleep=7
+
+
